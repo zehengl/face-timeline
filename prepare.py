a08c1e5fbf787e2a3233d6e850925d436bb15120
Zeheng Li <imzehengl@gmail.com>
2023-03-30 16:03:04 -0600

    clean up

--- prepare.py
+++ prepare.py
@@ -5,7 +5,7 @@ from os import getenv
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
-
+import matplotlib.pyplot as plt
 
 load_dotenv()
 
@@ -22,16 +22,19 @@ df["date"] = df["file"].apply(lambda x: pd.to_datetime(x.stem))
 df.to_pickle(output / "df.pkl")
 
 # %%
+plt.cla()
 ax = sns.countplot(df, x=df["date"].dt.year)
 ax.set(xlabel="Year")
 ax.get_figure().savefig("output/count-per-year.png", dpi=300, bbox_inches="tight")
 
 # %%
+plt.cla()
 ax = sns.countplot(df, x=df["date"].dt.month)
 ax.set(xlabel="Month")
 ax.get_figure().savefig("output/count-per-month.png", dpi=300, bbox_inches="tight")
 
 # %%
+plt.cla()
 ax = sns.countplot(
     df,
     x=df["date"].dt.day_name(),
